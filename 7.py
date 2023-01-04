2310fe73f92a039b19a4b6ff078eab09f84d479f
Nihar Hegde <59167599+nihar-hegde@users.noreply.github.com>
2023-01-04 17:14:44 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -6,34 +6,31 @@ import pandas as pd
 import numpy as np
 %matplotlib inline
 
-# import some data to play with
+
 iris = datasets.load_iris()
 
-# Store the inputs as a Pandas Dataframe and set the column names
+
 X = pd.DataFrame(iris.data)
 X.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
 
 y = pd.DataFrame(iris.target)
 y.columns = ['Targets']
 
-# K Means Cluster
-model = KMeans(n_clusters=3)
-model.fit(X) # This is what KMeans thought
-#model.labels_
 
-# Set the size of the plot
+model = KMeans(n_clusters=3)
+model.fit(X) 
+
+
+
 plt.figure(figsize=(14,14))
 
-# Create a colormap
 colormap = np.array(['red', 'lime', 'black'])
 
-# Plot the Original Classifications using Petal features
 plt.subplot(2, 2, 1)
 plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
 plt.title('Real Classification')
 plt.xlabel("Petal Length")
 plt.ylabel('Petal Width')
-# Plot the Models Classifications
 plt.subplot(2, 2, 2)
 plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40)
 plt.title('K Mean Classification')
@@ -48,7 +45,7 @@ scaler = preprocessing.StandardScaler()
 scaler.fit(X)
 xsa = scaler.transform(X)
 xs = pd.DataFrame(xsa, columns = X.columns)
-#xs.sample(5)
+
 
 from sklearn.mixture import GaussianMixture
 gmm = GaussianMixture(n_components=3)
